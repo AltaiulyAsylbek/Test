a10ca23b0b93213bc5b9e54a1ff41de53d47728c
AltaiulyAsylbek <altaiuly.asylbek@gmail.com>
2020-08-21 20:55:09 +0600

    third commit

---
+++ src/com/company/SampleClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class SampleClass {
+    private String lastName;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
